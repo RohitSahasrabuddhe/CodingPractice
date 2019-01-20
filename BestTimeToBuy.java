89857648e322d1911eced2663b8aba35609b8fff
Rohit Sahasrabuddhe <rohitbhalchandras@gmail.com>
2019-01-19 18:15:11 -0800

    BestTimeToBuy Optimal

--- BestTimeToBuy.java
+++ BestTimeToBuy.java
@@ -2,6 +2,32 @@ class BestTimeToBuy {
     public int maxProfit(int[] prices) {
         
         int length = prices.length;
+        if (length == 0) {
+            return 0;
+        }
+        int[] maxArray = new int[length];
+        int max = prices[length - 1];
+        
+        for (int i = length - 1; i >= 0; i--){
+            if (prices[i] > max) {
+                max = prices[i];
+            }
+            maxArray[i] = max;
+        }
+        
+        int maxDif = 0;
+        for (int i = 0; i < length; i++) {
+            int currentDif = maxArray[i] - prices[i];
+            if (currentDif > maxDif) {
+                maxDif = currentDif;
+            }
+        }               
+        
+        return maxDif;
+    }
+    public int maxProfitBruteForce(int[] prices) {
+        
+        int length = prices.length;
         int profit = 0 ; 
         int start = -1;
         int end = -1;
