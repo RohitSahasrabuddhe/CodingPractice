c96bb906065e8eba7e5e47bda7bdb967bde26e83
Rohit Sahasrabuddhe <rohitbhalchandras@gmail.com>
2019-02-09 18:40:43 -0800

    Faster approach for firstUnique char problem

--- FirstUniqueCharInString.java
+++ FirstUniqueCharInString.java
@@ -1,5 +1,5 @@
 class FirstUniqueCharInString {
-    public int firstUniqChar(String s) {
+    public int firstUniqCharNaive(String s) {
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             int j = 0;
@@ -13,6 +13,33 @@ class FirstUniqueCharInString {
             }
             if (j == s.length()) {
                 return i;
+            }
+        }
+        return -1;
+    }
+
+    public int firstUniqChar(String s) {
+        boolean[] flags = new boolean[26];
+        int[] vals = new int[26];
+        
+        Set<Character> set = new HashSet<Character>();
+        for (int i =0; i < s.length(); i++) {
+            if (flags[(int)(s.charAt(i) - 97)] == true) {
+                continue;
+            }
+            
+            if (set.contains(s.charAt(i))) {
+                flags[(int)(s.charAt(i) - 97)] = true;
+            }
+            else{
+                set.add(s.charAt(i));
+                vals[(int)(s.charAt(i) - 97)] = i;
+            }
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (flags[(int)(s.charAt(i) - 97)] == false) {
+                return vals[(int)(s.charAt(i) - 97)];
             }
         }
         return -1;
