bb802553bfe822d949d4dead2bc76a8fb5bb1fe6
Rohit Sahasrabuddhe <rohitbhalchandras@gmail.com>
2019-02-18 14:44:56 -0800

    Maximum depth of a binary tree  fastest

---
+++ MaximumDepthOfBinaryTree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class MaximumDepthOfBinaryTree {
+    public int depth = 0;
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        DFS(root,1);
+        return depth;
+    }
+    
+    public void DFS(TreeNode node, int currentDepth) {
+        if (currentDepth > depth)
+            depth = currentDepth;
+        
+        if (node.left != null){
+            DFS(node.left, currentDepth+1);
+        }
+        if (node.right != null) {
+            DFS(node.right, currentDepth+1);
+        }
+        return;
+    }
+}
