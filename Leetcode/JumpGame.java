93c032c7ee7ac52131f78f08ca3d282aaa4ae20d
Rohit Sahasrabuddhe <rohitbhalchandras@gmail.com>
2019-02-26 23:08:14 -0800

    Casn jump fastest approach

--- Leetcode/JumpGame.java
+++ Leetcode/JumpGame.java
@@ -21,4 +21,17 @@ class JumpGame {
         }
         return false;
     }
+
+    public boolean canJumpFastest(int[] nums) {
+        if(nums.length == 1) {
+            return true;
+        }        
+        int n = nums.length-1;            
+        for(int i = n-1; i >= 0; i--) {
+            if(nums[i] >= n-i){
+                n = i;
+            }
+        }        
+        return n == 0;
+    }
 }
