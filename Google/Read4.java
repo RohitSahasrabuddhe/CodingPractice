f55808e536c514f6363633cec22c424c871fc557
Rohit Sahasrabuddhe <rohitbhalchandras@gmail.com>
2019-03-06 21:43:22 -0800

    Efficient code

--- Google/Read4.java
+++ Google/Read4.java
@@ -9,28 +9,13 @@ public class Read4 extends Reader4 {
      * @return    The number of actual characters read
      */
     public int read(char[] buf, int n) {
-        char[] inBuf;
-        int size = n;
+        char[] inBuf = new char[4];
         int c = 0;
-        while(size > 4) {
-            inBuf = new char[4];
-            int retVal = read4(inBuf);
-            if(retVal < 4) {
-                // file is finished reading
-               for(int i = 0; i < retVal; i++) {
-                   buf[c++] = inBuf[i];
-                   return c;
-               } 
-            }
-            for(int  i = 0; i < 4; i++) {
-                buf[c++] = inBuf[i];
-            }
-            size -= 4;
-        }
-        inBuf = new char[4];
-        int retVal = read4(inBuf);
-        for(int i = 0; i < size && i < retVal; i++) {
-            buf[c++] = inBuf[i];
+        int retVal = 0;
+        while(c < n && (retVal = read4(inBuf)) > 0 ) {
+           for(int i = 0; i < retVal && c < n; i++) {
+               buf[c++] = inBuf[i];
+           } 
         }
         return c;
     }
