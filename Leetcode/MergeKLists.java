2ed4968be296e4fbffef1555390bbb534e83d2a3
Rohit Sahasrabuddhe <rohitbhalchandras@gmail.com>
2019-03-10 21:27:19 -0700

    Merge k sorted Lists using priority queue

--- Leetcode/MergeKLists.java
+++ Leetcode/MergeKLists.java
@@ -6,7 +6,33 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+import java.util.*;
 class Solution {
+    public ListNode mergeKListsWithHeap(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(5, new Comparator<ListNode>() {
+           @Override
+            public int compare(ListNode l1, ListNode l2) {
+                return (l1.val - l2.val);
+            }
+        });
+        
+        for(int i = 0; i < lists.length; i++) {
+            while(lists[i] != null) {
+                ListNode l = new ListNode(lists[i].val);
+                pq.add(l);
+                lists[i] = lists[i].next;
+            }
+        }
+        ListNode head = new ListNode(0);
+        ListNode op = head;
+        while(pq.size() > 0) {
+            ListNode l = pq.poll();
+            op.next = l;
+            op = op.next;
+        }
+        
+        return head.next;
+    }
     public ListNode mergeKLists(ListNode[] lists) {
         int n = lists.length;
         
