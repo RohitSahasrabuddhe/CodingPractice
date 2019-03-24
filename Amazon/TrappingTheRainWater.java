eae83c0948e573cda7a13d25a2e889d8c9308c71
Rohit Sahasrabuddhe <rohitbhalchandras@gmail.com>
2019-03-23 19:16:01 -0700

    trapping the rainwater 97% beats

--- Amazon/TrappingTheRainWater.java
+++ Amazon/TrappingTheRainWater.java
@@ -21,5 +21,30 @@ class Solution {
         }
         return op;
     }
+
+    public int trapWithTwoPointers(int[] height) {
+        int n = height.length;
+        if(n == 0){
+            return 0;
+        }
+        int[] lMax = new int[n];
+        int[] rMax = new int[n];
+        lMax[0] = 0;
+        rMax[n-1] = 0;
+        // generate lMax array
+        for(int i = 1; i < n; i++) {
+            lMax[i] = Math.max(lMax[i-1], height[i-1]);
+        }
+        
+        // generate rMax array
+        for(int i = n-2; i >= 0; i--) {
+            rMax[i] = Math.max(rMax[i+1], height[i+1]);
+        }
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            ans += Math.max((Math.min(lMax[i], rMax[i]) - height[i]), 0); 
+        }
+        return ans;
+    }
     
 }
