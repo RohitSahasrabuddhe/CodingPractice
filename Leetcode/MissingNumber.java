4cc60e264ddb665630989e88f8ad87950ce0752a
Rohit Sahasrabuddhe <rohitbhalchandras@gmail.com>
2019-03-31 20:23:55 -0700

    Missing number coller approach

--- Leetcode/MissingNumber.java
+++ Leetcode/MissingNumber.java
@@ -15,3 +15,15 @@ class MissingNumber {
         return 0;
     }
 }
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+        int actualSum = (n*(n+1))/2;
+        for(int num : nums){
+            sum += num;
+        }
+        return actualSum - sum;
+    }
+}
